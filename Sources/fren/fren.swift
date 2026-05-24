b9d31b67906f2b937268cc977e21470d24d8e176
Artem <artem@deffun.io>
2026-05-24 23:05:18 +0700

    add run subcommand

--- Sources/fren/fren.swift
+++ Sources/fren/fren.swift
@@ -1,11 +1,34 @@
 import ArgumentParser
+import Foundation
 
 @main
 struct fren: ParsableCommand {
-    @Argument(help: "The person to greet.")
-    public var name: String
+    static let configuration = CommandConfiguration(
+        subcommands: [hello.self, run.self]
+    )
+}
 
-    public func run() throws {
-        print("Hello fren, \(name)!")
+extension fren {
+    struct hello: ParsableCommand {
+        @Argument(help: "fren's name")
+        var name: String
+
+        func run() throws {
+            print("Hello fren \(name)!")
+        }
+    }
+}
+
+extension fren {
+    struct run: ParsableCommand {
+        @Argument(help: "js file path")
+        var path: String
+
+        func run() throws {
+            let base = URL(fileURLWithPath: FileManager.default.currentDirectoryPath, isDirectory: true)
+            let url = URL(fileURLWithPath: path, relativeTo: base)
+            let contents = try String(contentsOf: url, encoding: .utf8)
+            print(contents, terminator: "")
+        }
     }
 }
